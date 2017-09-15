b2263ffa596f13ea4172d0c81cf686af801e2b2f
Jeremie Francois (on alpha) <jeremie.francois@gmail.com>
2017-09-15 19:22:06 +0200

    Created by gitinit 20170915-192206

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+if($_SERVER["SERVER_ADDR"]!='::1' && (!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] != 'on'))
+{
+    header('Location: https://www.tecrd.com/td');
+    exit;
+}
+/*
+	This file is part of myTinyTodo.
+	(C) Copyright 2009-2010 Max Pozdeev <maxpozdeev@gmail.com>
+	Licensed under the GNU GPL v2 license. See file COPYRIGHT for details.
+*/
+
+require_once('./init.php');
+
+$lang = Lang::instance();
+
+if($lang->rtl()) Config::set('rtl', 1);
+
+if(!is_int(Config::get('firstdayofweek')) || Config::get('firstdayofweek')<0 || Config::get('firstdayofweek')>6) Config::set('firstdayofweek', 1);
+
+define('TEMPLATEPATH', MTTPATH. 'themes/'.Config::get('template').'/');
+
+require(TEMPLATEPATH. 'index.php');
+
+?>
